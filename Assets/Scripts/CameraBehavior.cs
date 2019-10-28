38c9a41b0e18f2c7bf77332ed1c78f391cee9cfd
Julia Gopachenko <yulia.gopachenko@gmail.com>
2019-10-28 18:53:22 +0300

    Create base player and camera

---
+++ Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = player.transform.position - transform.position;
+    }
+
+    // Camera follow to player
+    void Update()
+    {
+        transform.position = player.transform.position - offset;
+    }
+
+    //Установить вид камеры сверху на финише
+    void ViewFromAbove()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    private float distToGround;
+
+    public float forwardForce = 400f;
+    public float upForce = 10f;
+    public float sidewaysForce = 10f;
+
+    public Collider playerCollider;
+    public Rigidbody playerRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distToGround = playerCollider.bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //доб. силу тв.телу
+        //rigitBode.useGravity = false - чтобы не крутился
+        playerRb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        //прыжок
+        if (Input.GetKeyDown(KeyCode.W) && isUpForce())
+        {
+            playerRb.AddForce(0, upForce, forwardForce * Time.deltaTime);
+        }
+
+        //движение вправо, влево
+        if (Input.GetKey(KeyCode.D)) //If the player is pressing "d" key
+        {
+            playerRb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey(KeyCode.A)) //If the player is pressing "a" key
+        {
+            playerRb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+
+    //проверяет расстояние до платформы
+    //для допуска прыжка, чтобы не было doube jumps
+    private bool isUpForce()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
